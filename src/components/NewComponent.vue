c59d0d969597fe93bf66c5909ebcc4370cc31099
caiocroccia22 <caiocroccia22@gmail.com>
2023-11-19 15:59:01 -0300

    Criação do component do 0

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,33 @@
+<!--
+    
+    Lembrando que o component no Vue
+    possui 3 elementos para inserir 
+    1) Template
+
+
+-->
+
+<template>
+    <!--Elemento que corresponde ao HTML-->
+    <h1>Bem vindo ao meu App Vue</h1>
+</template>
+
+<script>
+    //Elemento que compreende ao Javascript
+    //Como nosso component está isolado temos que exportar ele
+    export default {
+        name: 'Newcomponent.vue',
+        props: {
+            texto: String
+        }
+    }
+</script>
+
+
+<style scoped>
+    /*Elemento que compreende ao CSS*/
+    h1{
+        color: red
+    }
+    /*Scoped é para atribuir a folha de estilo somente a esse component*/
+</style>
